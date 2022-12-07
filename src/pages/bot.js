f69cc9f3e657b1fbbbb82e201084d2fd4223cc3b
xiaohuangniu <xiaohuangniu@example.com>
2022-12-07 14:15:27 +0800

    fix

--- src/pages/bot.js
+++ src/pages/bot.js
@@ -51,7 +51,6 @@ function BotPage(){
   const [choiceAddressIndex,setChoiceAddressIndex] = useState(-1)
   const [logs,setLogs] = useState([])
   const [isPending,setIsPending] = useState(false)
-  const [isSell,setIsSell] = useState(false)
 
   // const WBNBAddress = "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd"
   // const routerAddress = "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3"
@@ -64,13 +63,16 @@ function BotPage(){
   useEffect(() => {
     console.log("isRun",isRun,isPending)
     if (isRun && !isPending) {
+
       const timer = setInterval(async () => {
+        const gas = ((await bnbProvider.getGasPrice()))
+        const gasPrice = ethers.utils.formatEther(gas.mul(310000))
         if (addressList) {
           const buyBNB = _.floor(_.random(minBNB,maxBNB,true),4)
           setNextBuyBNB(buyBNB)
           let tempAddressList = []
           for (let i = 0;i< addressList.length;i++) {
-            if (parseFloat(addressList[i].balance) > parseFloat(buyBNB) ){
+            if (parseFloat(addressList[i].balance) > parseFloat(buyBNB)+ parseFloat(gasPrice) ){
               tempAddressList.push(addressList[i])
             }
           }
@@ -280,11 +282,14 @@ function BotPage(){
         tokenAddress,
         [
           'function balanceOf(address account) public view virtual returns (uint256)'],
+        wallet0.wallet.connect(bnbProvider)
       )
       let tokenBalance = await tokenContractObj.balanceOf(wallet0.wallet.address)
 
 
-      addressList[choiceAddressIndex] = {
+
+      var newAddressList = [...addressList]
+      newAddressList[choiceAddressIndex] =  {
         wallet:wallet0.wallet,
         address:wallet0.address,
         balance:balanceInBNB,
@@ -292,7 +297,8 @@ function BotPage(){
         wbnb:0,
         tokenAmount:tokenBalance.toString(),
       }
-      setAddressList(addressList)
+
+      setAddressList(newAddressList)
       return true
   }
 
@@ -306,8 +312,12 @@ function BotPage(){
   const [sellDelay, setSellDelay] = useState(1000)
   const [isSellRun,setIsSellRun] = useState(false)
   const [isSellPending,setIsSellPending] = useState(false)
+  const [nextSellToken,setNextSellToken] = useState(0)
+
+
   const updateSellIsRun = ()=> {
-    if (!isRun) {
+    console.log("llll",isSellRun)
+    if (!isSellRun) {
       if (maxToken <= 0 || minToken <= 0 || buyMaxTokenSecond <= 0 || buyMinTokenSecond <= 0 ){
         toast({
           title: '参数错误',
@@ -320,25 +330,29 @@ function BotPage(){
         return;
       }
     }
-    setIsSellRun(!isRun)
+    setIsSellRun(!isSellRun)
     setIsSellPending(false)
   }
 
   useEffect(() => {
-    console.log("isRun",isRun,isPending)
+    console.log("isSellRun",isSellRun,isSellPending)
     if (isSellRun && !isSellPending) {
 
       const timer = setInterval(async () => {
         console.log(addressList)
+        const gas = ((await bnbProvider.getGasPrice()))
         if (addressList) {
           // random buyToken
-          const buyToken = _.floor(_.random(minToken,maxToken,false),0)
-          setNextBuyBNB(buyToken)
+          const sellToken = _.floor(_.random(minToken,maxToken,false),0)
+          setNextSellToken(sellToken)
 
           let tempAddressList = []
           for (let i = 0;i< addressList.length;i++) {
-            console.log("ccc",parseFloat(addressList[i].tokenAmount) ,parseFloat(buyToken)*Math.pow(10,decimals),Math.pow(10,decimals) )
-            if (parseFloat(addressList[i].tokenAmount) > parseFloat(buyToken)*Math.pow(10,decimals) ){
+            const curBalance = ethers.utils.parseUnits(String(addressList[i].balance), 'ether');
+            console.log("curBalance",curBalance.toString(),gas.mul(310000).toString())
+            if (parseFloat(addressList[i].tokenAmount) > parseFloat(sellToken)*Math.pow(10,decimals) &&
+              curBalance.gt(gas.mul(310000))
+            ){
               tempAddressList.push(addressList[i])
             }
           }
@@ -359,7 +373,7 @@ function BotPage(){
 
 
             logs.push("swap"+second+"秒即将开始,交易金额为"
-              +buyToken+"token ,"+"用户地址为:"+addressList[index].address
+              +sellToken+"token ,"+"用户地址为:"+addressList[index].address
               +"用户bnb余额为:"+addressList[addressIndex].balance+"用户token amount:"
               +addressList[addressIndex].tokenAmount)
 
@@ -367,7 +381,7 @@ function BotPage(){
             setIsSellPending(true)
           }
         }
-      }, delay)
+      }, sellDelay)
       return () => clearInterval(timer)
     }
   }, [sellDelay,isSellRun,isSellPending])
@@ -426,7 +440,7 @@ function BotPage(){
       ],
       account
     )
-    const amountIn = ethers.utils.parseUnits(String(nextBuyBNB), decimals).add(10);
+    const amountIn = ethers.utils.parseUnits(String(nextSellToken), decimals).add(10);
     // console.log(String(nextBuyBNB),amountIn)
     // const amountIn = new BigNumber(String(nextBuyBNB))
     // 0xa6c8b55c8fc30b9448367f18c59f87cccb4a8de3 替换自己的合约地址
@@ -463,14 +477,16 @@ function BotPage(){
     )
     let tokenBalance = await tokenContractObj.balanceOf(wallet0.address)
 
-    addressList[choiceSellAddressIndex] = {
+
+    var newAddressList = [...addressList]
+    newAddressList[choiceSellAddressIndex] = {
       wallet:wallet0.wallet,
       tokenAmount:tokenBalance.toString(),
       address:wallet0.address,
       balance:balanceInBNB,
       index:wallet0.index, //fix
     }
-    setAddressList(addressList)
+    setAddressList(newAddressList)
     return true
   }
 
@@ -483,9 +499,7 @@ function BotPage(){
           const res = await approveToken(choiceSellAddressIndex)
           console.log(res)
           if (res) {
-            console.log("dd")
-            const sr = await swapToken(isSellPending)
-            console.log("ddd")
+            const sr = await swapToken(choiceSellAddressIndex)
             if (sr) {
               setIsSellPending(false)
               logs.push("本次swapToken结束")
@@ -660,8 +674,8 @@ function BotPage(){
         </NumberInputStepper>
       </NumberInput>
 
-      <Button disabled={isRun} mt={2} colorScheme={'blue'} onClick={()=>{updateSellIsRun()}}>确定卖出配置</Button>
-      {isRun?  <Button mt={2} colorScheme={'blue'} onClick={()=>{updateSellIsRun()}}>停止卖出运行</Button>:""}
+      <Button disabled={isSellRun} mt={2} colorScheme={'blue'} onClick={()=>{updateSellIsRun()}}>确定卖出配置</Button>
+      {isSellRun?  <Button mt={2} colorScheme={'blue'} onClick={()=>{updateSellIsRun()}}>停止卖出运行</Button>:""}
 
 
       <chakra.div mt={2} borderRadius={"0.325rem"} overflow={'auto'} bg={'purple.200'} p={2}>
