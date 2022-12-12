b847668fc3eb466ec51d1b65fe1e2e6a0d18e462
xiaohuangniu <xiaohuangniu@example.com>
2022-12-12 22:17:11 +0800

    fix bug

--- src/pages/bot.js
+++ src/pages/bot.js
@@ -223,8 +223,9 @@ function BotPage(){
         console.log('Transaction receipt',receipt)
       }
 
+    const gas = ((await bnbProvider.getGasPrice()))
     const result = await bnbProvider.getBalance(wallet0.address)
-    const balanceInBNB = ethers.utils.formatEther(result)
+    const balanceInBNB = ethers.utils.formatEther(result.sub(gas.mul(41000)))
 
     let wbnbBalance = await wbnb.balanceOf(wallet0.address)
     addressList[choiceAddressIndex] = {wallet:wallet0.wallet,address:wallet0.address,balance:balanceInBNB,index:wallet0.index,wbnb:ethers.utils.formatEther(wbnbBalance)}
@@ -270,9 +271,11 @@ function BotPage(){
       );
       const receipt = await tx.wait()
       console.log('Swap receipt',receipt)
-
+    const gas = ((await bnbProvider.getGasPrice()))
       const result = await bnbProvider.getBalance(wallet0.address)
-      const balanceInBNB = ethers.utils.formatEther(result)
+
+    const balanceInBNB = ethers.utils.formatEther(result.sub(gas.mul(41000)))
+
       // const wbnb = new ethers.Contract(
       //   WBNBAddress,
       //   `[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}]`,
@@ -468,9 +471,11 @@ function BotPage(){
     );
     const receipt = await tx.wait()
     console.log('Swap receipt',receipt)
-
     const result = await bnbProvider.getBalance(wallet0.address)
-    const balanceInBNB = ethers.utils.formatEther(result)
+    const gas = ((await bnbProvider.getGasPrice()))
+
+
+    const balanceInBNB = ethers.utils.formatEther(result.sub(gas.mul(41000)))
 
     const tokenContractObj = new ethers.Contract(
       tokenAddress,
